2657217e12dd960ae1c67ec25090c2fce2a6185b
Joymangolch <147248110+Joymangolch@users.noreply.github.com>
2023-12-15 14:00:37 +0530

    added

---
+++ add.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,sum;
+	printf("Enter any integer : ");
+	scanf("%d",&a);
+	printf("Enter any integer : ");
+        scanf("%d",&b);
+
+	sum = a + b;
+	
+	printf("The sum of two no. is : %d",sum);
+
+	return 0;
+
+
+}
